204fe6a64e4ac98575bf304ed339e15b7e37f10e
Lion <craig.m.lindahl@gmail.com>
2020-03-17 09:42:26 -0700

    Serving html successfully

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const path = require ('path');
 const app = express();
 const port = 1028;
 
-app.get('/', (req, res) => res.send('You Got Served'))
+// app.get('/', (req, res) => res.send('You Got Served'))
+
+app.use(express.static(path.join(__dirname, '../client', 'public')))
 
 app.listen(port, () => console.log(`And Now Its On Port ${port}`))
